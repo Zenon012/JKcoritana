f58bc7879cfa96d0243f1e85aa5ece99af03adf8
Zenon012 <56444885+Zenon012@users.noreply.github.com>
2019-10-13 09:49:09 +0800

    Add files via upload
    Easy to Hard

---
+++ questionDB.py
@@ -0,0 +1,29 @@
+def questionsEasy(): #Simple Interest
+    questions=["empty","Find the interest and amount on $800 at 6 and a half percent simple interest for 5 years?","Find the interst and amount on $900 at 7 1/4% simple interest for 9 months.","Find the Interest and amount on $1200 at 11 3/8% simple interst for 2 years and 5 months.","If a principal earns interest of $385 in 2 years and 9 months at a simple interest rate of 9 1/2%. Find the principal invested.","How long will it take for $8000 to earn $2400 if it is invested at 6 1/2% simple interest?"] 
+    A=["empty","A.)I = 260 and F = $1060","B.)I = 553 and F = $882","C.)I = 422 and F = $1560","D.)I = 544 and F = $9696","E.)I = 777 and F = $2334"] 
+    B=["empty","A.)II = 30.23 and F = $728.33","B.)I = 48.94 and F = $948.94","C.)I = 42.55 and F = $122.35","D.)I = 25.34 and F = $152.60","E.)I = 21.12 and F = $155.23"]
+    C=["empty","A.)II = 422.223 and F = $1560.33","B.)I = 328.864 and F = $1696.221","C.)I = 329.875 and F = $1529.875","D.)I = 328.223 and F = $1529.745","E.)I = 331.875 and F = $1530.875"]
+    D=["empty","A.)IInterest rate is 5%","B.)Interest rate is 3.48%","C.)Interest rate is 8.52%","D.)Interest rate is 2.28%","E.)Interest rate is 2.29%"]
+    E=["empty","A.)I$1453.70","B.)$1573.68","C.)$2073.78","D.)$3473.90","E.)$1473.68"]
+    correct=["empty","A.)I = $260 and F = $1060","B.)I = 48.94 and F = $948.94","C.)I = 329.875 and F = $1529.875","D.)Interest rate is 2.28%","E.)$1473.68"]
+
+def questionsMedium(): #Chemistry
+	questions=["empty","it can be measured and observed without changing the composition of the substance.","it can be measured and observed by performing chemical change","it is a substance that cannot be seperated into simpler substances by chemical means.","Which of these is not part of Physical Properties","Which of these is not part of Chemical Properties"] 
+	A=["empty","A.)Solid","B.)Compound","C.)Mixture","D.)Physical Properties","E.)Chemical Properties"] 
+	B=["empty","A.)Components","B.)Extensive","C.)Chemical Properties","D.)Physical Properties","E.)Homogenous Mixture"]
+	C=["empty","A.)Elements","C.)Compounds","D.)Distillation","D.)Matter","E.)Density"]
+	D=["empty","A.)Mass","B.)Flammability","C.)Volume","D.)Weight","E.)Density"]
+	E=["empty","A.)Toxicity","B.)Radioactivity","C.)Heat of Combustion","D.)Flammability","E.)Temperature"]
+	tama=["empty","D.)Physical Properties","C.)Chemical Properties","A.)Elements","B.)Flammability","E.)Temperature"]
+
+def questionsHard(): #Physics
+	questions=["empty","A formula that consist of Total Displacement over time","Travelling from point A to point B, Harvey moved constantly at 10.0 ft/s. He then travelled back from point B to point A at a constant speed of 6.0 ft/s. What is his Average speed and Velocity?","A formula that consists of Change in velocity over a period of time.","A car speeds up from 20 m/s in 4 seconds as it passes a truck. What is its acceleration?","An airplane accelerates down a runway at 3.20 m/s^2 for 32.8 s until is finally lifts off the ground. Determine the distance traveled before takeoff."] 
+	A=["empty","A.)Average Speed","B.)Average Velocity","C.)Total Distance","D.)","E.)Total Velocity"] 
+	B=["empty","A.)30/4 ft/s","B.)5.5ft/s","C.)9.8ft/s","D.)15/2 ft/s","E.)7.5ft/s"]
+	C=["empty","A.)Acceleration","B.)Freefall","C.)Projectile Motion","D.)Speed","E.)Deceleration"]
+	D=["empty","A.)7.4 m/s^2","B.)8/6 m/s^2","C.)9/10 m/s^2","D.)7/4 m/s^2","E.)8.1 m/s^2"]
+	E=["empty","A.)d = 1690 m","B.)d = 1736 m","C.)d = 1720 m","D.)d = 1029 m","E.)d = 1721 m"]
+	tama=["empty","B.)Average Velocity","E.)7.5ft/s","A.)Acceleration","D.)7/4 m/s^2","C.)d = 1720 m"]
+
+
+
